dea7cc575d7449c8f2aa490e1024cd3ae027323d
Luis Osvaldo <aldolp40@gmail.com>
2019-04-18 13:06:42 -0500

    beta2

--- js/main.js
+++ js/main.js
@@ -22,30 +22,6 @@ function showQRIntro() {
 }
 
 function openQRCamera(node) {
-<<<<<<< HEAD
-    var reader = new FileReader();
-    reader.onload = function() {
-      node.value = "";
-      qrcode.callback = function(res) {
-        if(res instanceof Error) {
-          alert("No QR code found. Please make sure the QR code is within the camera's frame and try again.");
-        } else {
-          node.parentNode.previousElementSibling.value = res
-          alert('success!')
-          var url = " " + $("#text").val();
-          window.open(url);
-        }
-      };
-      qrcode.decode(reader.result);
-    };
-    reader.readAsDataURL(node.files[0]);
-  }
-
-//   $("#btn").click( function() {
-//     var url = " " + $("#text").val();
-//     window.open(url);
-// });
-=======
   var reader = new FileReader();
   reader.onload = function() {
     node.value = "";
@@ -72,4 +48,3 @@ function openQRCamera(node) {
     var url = " " + $("#text").val();
     window.open(url);
 });
->>>>>>> osvo
